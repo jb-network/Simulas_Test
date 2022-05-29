d1f3cbc0393e56986295ffcc5c71370ce025b3cd
jb-network <85036487+jb-network@users.noreply.github.com>
2022-05-29 11:38:56 -0500

    Add project files.

---
+++ Simulas_Test/Program.cs
@@ -0,0 +1,9 @@
+﻿// This is challenge work for the "C# Players Guide"
+// Level 16 Simula's Test Challenge
+// The goal is to use enumeration for the state of a chest
+//The chest has four three states OPEN <> CLOSED <> LOCKED
+//The user can do the following actions (in lowercase) OPEN > close > CLOSED, OPEN < open < CLOSED, CLOSED > locked > LOCKED, CLOSED < unlocked < LOCKED
+
+
+
+
