3fffabcbedf635afd8de9e242d989e3c52edd40a
jb-network <85036487+jb-network@users.noreply.github.com>
2022-05-29 18:12:03 -0500

    code finished

--- Simulas_Test/Program.cs
+++ Simulas_Test/Program.cs
@@ -4,23 +4,67 @@
 //The chest has four three states OPEN <> CLOSED <> LOCKED
 //The user can do the following actions (in lowercase) OPEN > close > CLOSED, OPEN < open < CLOSED, CLOSED > locked > LOCKED, CLOSED < unlocked < LOCKED
 
-
-//Main
-// WHILE TRUE ADD
-
 // Set up
-// ChestState Chest = ChestState.LOCKED;
+ChestState Chest = ChestState.LOCKED;
 
-//Console.WriteLine($"The chest is {Chest}.  What do you want to do?"
-ChestState ChestAction();
+while (true)
+{
+    string menu = UserInteraction(Chest);
+    Chest = ChestChange(menu, Chest);
 
-//Methods
-//ADD METHOD FOR CHEST STATE, RETURN STATUS
 
-//ADD USER ACTION
 
-//ADD VALIDATE USER INPUT
+    //Methods -----------------
 
-// Enumerant
+    //Provides status and captures user's input
+    //Convert to lower to capture user input errors on word
+    //Other input errors will be handled by StateChange method
+    string UserInteraction(ChestState Chest)
+    {
+        Console.WriteLine($"\nThe chest is {Chest}.  What do you want to do?");
+        Console.WriteLine("Actions are based on status: " +
+            "\n1) Chest Status: 'OPEN'       Enter: 'close' to change to'CLOSED'" +
+            "\n2) Chest Status: 'CLOSED'     Enter: 'lock' to change to 'LOCKED'" +
+            "\n3) Chest Status: 'LOCKED      Enter: 'unlock' to change to 'CLOSED'" +
+            "\n4) Chest Status: 'CLOSED      Enter: 'open' to change to 'OPEN'");
+        Console.Write("Input your action: ");
+        string action = Console.ReadLine().ToLower();
+        return action;
+    }
+
+    //Checks chest and use selection to change chest status
+    //Error checking provided by else statment.  If the user's input was no correct error message provide and chest is not change.
+    ChestState ChestChange(string menu, ChestState Chest)
+    {
+        if (Chest == ChestState.OPEN && menu == "close")
+        {
+            Chest = ChestState.CLOSED;
+            return Chest;
+        }
+        else if (Chest == ChestState.CLOSED && menu == "lock")
+        {
+            Chest = ChestState.LOCKED;
+            return Chest;
+        }
+        else if (Chest == ChestState.LOCKED && menu == "unlock")
+        {
+            Chest = ChestState.CLOSED;
+            return Chest;
+        }
+        else if (Chest == ChestState.CLOSED && menu == "open")
+        {
+            Chest = ChestState.OPEN;
+            return Chest;
+        }
+        else
+        {
+            Console.WriteLine($"\nYou entered {menu} for the status of {Chest}. This is not a correct option, please try again!");
+            return Chest;
+        }
+    }
+}
+
+
+// Enumerant -----------------
 enum ChestState { OPEN, CLOSED, LOCKED }
 
